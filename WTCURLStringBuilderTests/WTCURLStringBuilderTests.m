09bed77a3788645ea72867e639b6d24c99f706ff
Andrew Carter <andrew.r.carter@gmail.com>
2013-10-15 10:42:35 -0400

    Initial Commit

---
+++ WTCURLStringBuilderTests/WTCURLStringBuilderTests.m
@@ -0,0 +1,34 @@
+//
+//  WTCURLStringBuilderTests.m
+//  WTCURLStringBuilderTests
+//
+//  Created by Andrew Carter on 10/15/13.
+//  Copyright (c) 2013 WillowTree Apps. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WTCURLStringBuilderTests : XCTestCase
+
+@end
+
+@implementation WTCURLStringBuilderTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
